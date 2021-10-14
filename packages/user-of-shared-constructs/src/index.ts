1c96b81c7dbbe923a917021eba4f1749e6a2b948
Ben Limmer <ben@benlimmer.com>
2021-10-14 14:20:40 -0600

    show main working

--- packages/user-of-shared-constructs/src/index.ts
+++ packages/user-of-shared-constructs/src/index.ts
@@ -1,6 +1,6 @@
 // TODO: ideally I'd import like this, but I get this error:
 // Cannot find module 'shared-constructs/MyConstruct' or its corresponding type declarations.ts(2307)
-import { MyConstruct } from "shared-constructs/MyConstruct";
+import { MyConstruct } from "shared-constructs";
 
 // This works, though.
 // import { MyConstruct } from "shared-constructs/src/MyConstruct";
